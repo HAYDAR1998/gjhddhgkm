9aa784ac8ae8fa87d8875b0ac1a1aefb56f14ef2
HAYDAR1998 <44063096+HAYDAR1998@users.noreply.github.com>
2018-10-27 10:59:51 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '#'
+
+
+
+
+
+
+
+client.on('ready', () => {
+   client.user.setGame("Light Support .");
+}); 
+
+
+
+	
+
+client.on ("guildMemberAdd", member => {
+  
+   var role = member.guild.roles.find ("name", "Light Members.");
+   member.addRole (role);
+  
+})
+
+client.on ("guildMemberRemove", member => {
+   
+})
+
+
+
+
+
+	
+	
+	
+	client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find('name', 'chat');
+  if (!channel) return;
+  channel.send(`**Welcome To Server Light Support .** ${member} `);
+});
+
+
+
+client.login(process.env.BOT_TOKEN);
