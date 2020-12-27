af08ea726ab5b9ddaa8bba734783d8ea9081c1bb
HAYDAR1998 <44063096+HAYDAR1998@users.noreply.github.com>
2020-12-28 02:16:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,43 +1,108 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '#'
+const moment = require("moment")
+const fs = require("fs")
+ 
 
-
-
-
-
-
-const devs = ['450919463106248704'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-
-if (message.content.startsWith('pl')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**Done Change Account Playing To : :small_blue_diamond:**
-	__${argresult}__ :ballot_box_with_check:`)
-} else
-  if (message.content.startsWith('ad')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**Done Change Account Avatar :ballot_box_with_check: ${argresult}**`);
-      } else
-if (message.content.startsWith('st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/haydaraliraqistream");
-    message.channel.sendMessage(`**Done Change Account Streaming To : :small_blue_diamond:**
-	__${argresult}__ :ballot_box_with_check:`)
-  } else
-  if (message.content.startsWith('wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**Done Change Account Watching To : :small_blue_diamond:**
-	  __${argresult}__ :ballot_box_with_check:`)
-  } else
-  if (message.content.startsWith('ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**Done Change Account Listening To : :small_blue_diamond:**
-	   __${argresult}__ :ballot_box_with_check:`)
-  }
-
+ 
+client.on('guildMemberAdd', member => {
+member.guild.fetchInvites().then(guildInvites => {
+const ei = invites[member.guild.id];
+invites[member.guild.id] = guildInvites;
+const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+const inviter = client.users.get(invite.inviter.id);
+const logChannel = member.guild.channels.find(channel => channel.name === `oo`);
+if(!logChannel) return;
+setTimeout(() => {
+logChannel.send(`**Invited By: <@${inviter.id}>**`);
+},2000)
 });
+});
+ 
+client.on('guildMemberAdd',async member => {
+const Canvas = require('canvas');
+const jimp = require('jimp');
+const w = ['./welcome_4.png'];
+let Image = Canvas.Image,
+    canvas = new Canvas(800, 300),
+    ctx = canvas.getContext('2d');
+ctx.patternQuality = 'bilinear';
+ctx.filter = 'bilinear';
+ctx.antialias = 'subpixel';
+ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+ctx.shadowOffsetY = 2;
+ctx.shadowBlur = 2;
+ctx.stroke();
+ctx.beginPath(); 
+fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+    if (err) return console.log(err);
+    let BG = Canvas.Image;
+    let ground = new Image;
+    ground.src = Background;
+    ctx.drawImage(ground, 0, 0, 800, 300);
+ 
+})
+ 
+        let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
+        jimp.read(url, (err, ava) => {
+            if (err) return console.log(err);
+            ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+         if (err) return console.log(err);
+ 
+  ctx.font = '36px Arial';
+  ctx.fontSize = '72px';
+  ctx.fillStyle = "#ffffff";
+  ctx.textAlign = "center";
+  ctx.fillText(member.user.username, 545, 177);
+ 
+  ctx.font = '16px Arial Bold';
+  ctx.fontSize = '72px';
+  ctx.fillStyle = "#ffffff";
+  ctx.textAlign = "center";
+  ctx.fillText(`${member.guild.memberCount} Members`, 580, 200);
+ 
+  let Avatar = Canvas.Image;
+  let ava = new Avatar;
+  ava.src = buf;
+  ctx.beginPath();
+  ctx.arc(169.5, 148, 126.9, -100, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.clip();
+  ctx.drawImage(ava, 36, 21, 260, 260);
+   
+  let c = member.guild.channels.find('name', `oo`)
+  if(!c) return;
+  c.sendFile(canvas.toBuffer());
+ 
+});
+});
+});
+ 
+const invites = {};
+ 
+const wait = require('util').promisify(setTimeout);
+ 
+client.on('ready', () => {
+wait(1000);
+ 
+client.guilds.forEach(g => {
+g.fetchInvites().then(guildInvites => {
+invites[g.id] = guildInvites;
+});
+});
+});
+client.on ("guildMemberAdd", member => {
+  
+   var role = member.guild.roles.find ("name", "..");
+   member.addRole (role);
+  
+})
+
+client.on ("guildMemberRemove", member => {
+   
+})
+
 
 
 
