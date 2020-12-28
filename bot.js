608e147f0d2c9da2468ed74d8055ee014ddde412
HAYDAR1998 <44063096+HAYDAR1998@users.noreply.github.com>
2020-12-28 03:12:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,92 +8,40 @@ const fs = require("fs")
  
 
  
-client.on('guildMemberAdd', member => {
-member.guild.fetchInvites().then(guildInvites => {
-const ei = invites[member.guild.id];
-invites[member.guild.id] = guildInvites;
-const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-const inviter = client.users.get(invite.inviter.id);
-const logChannel = member.guild.channels.find(channel => channel.name === `general`);
-if(!logChannel) return;
-setTimeout(() => {
-logChannel.send(`**Invited By: <@${inviter.id}>**`);
-},2000)
-});
-});
- 
-client.on('guildMemberAdd',async member => {
-const Canvas = require('canvas');
-const jimp = require('jimp');
-const w = ['./welcome_4.png'];
-let Image = Canvas.Image,
-    canvas = new Canvas(800, 300),
-    ctx = canvas.getContext('2d');
-ctx.patternQuality = 'bilinear';
-ctx.filter = 'bilinear';
-ctx.antialias = 'subpixel';
-ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
-ctx.shadowOffsetY = 2;
-ctx.shadowBlur = 2;
-ctx.stroke();
-ctx.beginPath();
- 
-fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-    if (err) return console.log(err);
-    let BG = Canvas.Image;
-    let ground = new Image;
-    ground.src = Background;
-    ctx.drawImage(ground, 0, 0, 800, 300);
- 
-})
- 
-        let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
-        jimp.read(url, (err, ava) => {
-            if (err) return console.log(err);
-            ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-         if (err) return console.log(err);
- 
-  ctx.font = '36px Arial';
-  ctx.fontSize = '72px';
-  ctx.fillStyle = "#ffffff";
-  ctx.textAlign = "center";
-  ctx.fillText(member.user.username, 545, 177);
- 
-  ctx.font = '16px Arial Bold';
-  ctx.fontSize = '72px';
-  ctx.fillStyle = "#ffffff";
-  ctx.textAlign = "center";
-  ctx.fillText(`${member.guild.memberCount} Members`, 580, 200);
- 
-  let Avatar = Canvas.Image;
-  let ava = new Avatar;
-  ava.src = buf;
-  ctx.beginPath();
-  ctx.arc(169.5, 148, 126.9, -100, Math.PI * 2, true);
-  ctx.closePath();
-  ctx.clip();
-  ctx.drawImage(ava, 36, 21, 260, 260);
-   
-  let c = member.guild.channels.find('name', `general`)
-  if(!c) return;
-  c.sendFile(canvas.toBuffer());
- 
-});
-});
-});
- 
-const invites = {};
- 
-const wait = require('util').promisify(setTimeout);
- 
-client.on('ready', () => {
-wait(1000);
- 
-client.guilds.forEach(g => {
-g.fetchInvites().then(guildInvites => {
-invites[g.id] = guildInvites;
-});
-});
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+	if (!channel) return;
+
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	// Slightly smaller text placed above the member's display name
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+	// Add an exclamation point here and below
+	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`Welcome to the server, ${member}!`, attachment);
 });
 
 client.on('ready', () => {
